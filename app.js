0c11f879730246b5de6e695b3487e462eac1a58b
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-05-21 07:48:24 +0530

    Basic MVC setup along with add button implementation

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//Javascript
+var Model = (function() {
+
+})();
+
+var View = (function() {
+
+})();
+
+var Controller = (function(model, view) {
+    var type, description, amount;
+
+    document.querySelector(".add__btn").addEventListener("click", function() {
+        type = document.querySelector(".add__type").value;
+        description = document.querySelector(".add__description").value;
+        amount = document.querySelector(".add__value").value;
+    })
+    
+    return {
+        init: function(){
+            console.log("application has started");
+        }
+    }
+
+})(Model, View);
+
+Controller.init();
